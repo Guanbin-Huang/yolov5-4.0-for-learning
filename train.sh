c69531a329461384c847c4d0e3944d69db7fad86
PeterHuang <qsmy122011@gmail.com>
2021-10-27 14:10:51 +0000

    the momoment you can run yolov5 without any bug to stop it

---
+++ train.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python train.py --data data/voc.yaml --cfg models/yolov5s_for_voc2007.yaml --weights weights/yolov5s.pt --batch-size 16 --epochs 200
--- yolov5-4.0-for-learning/README.md
+++
@@ -1,2 +0,0 @@
-# yolov5_rewrite_for_learning
-for the purpose of learning
